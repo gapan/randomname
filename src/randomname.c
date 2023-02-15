0a5a9f6aaa16ca852647075186d3923c96651ee6
George Vlahavas <vlahavas@gmail.com>
2023-02-15 21:32:04 +0200

    Check for all long options
    Even if we don't have to handle every one of them, some just set flags
    and others are handled by the respective short option parsing anyway.

--- src/randomname.c
+++ src/randomname.c
@@ -14,14 +14,17 @@ void usage() {
 
 int main(int argc, char** argv) {
 
+    // flagged (value = 0) if the respective part is enabled
     int left_flag = 0;
     int middle_flag = 1;
     int right_flag = 0;
 
+    // 0 means that names could start with any letter
     char left_start = 0;
     char middle_start = 0;
     char right_start = 0;
 
+    // flagged (value = 0) if the respective option is enabled
     int ubuntu_flag = 1;
     int docker_flag = 1;
     int no_dashes_flag = 1;
@@ -44,10 +47,10 @@ int main(int argc, char** argv) {
         { "left-start",   required_argument, NULL,            0  },
         { "middle-start", required_argument, NULL,            0  },
         { "right-start",  required_argument, NULL,            0  },
-        { "separator",    required_argument, NULL,           's' },
-        { "ubuntu",       optional_argument, &ubuntu_flag,    0  },
         { "docker",       no_argument,       &docker_flag,    0  },
+        { "ubuntu",       optional_argument, &ubuntu_flag,    0  },
         { "no-dashes",    no_argument,       &no_dashes_flag, 0  },
+        { "separator",    required_argument, NULL,           's' },
         { NULL,           0,                 NULL,            0  } 
     };
 
@@ -55,8 +58,17 @@ int main(int argc, char** argv) {
         next_opt = getopt_long(argc, argv, short_options, long_options, NULL);
         switch (next_opt) {
             case 0: // A long-only option
+                // these first ones are handled by their respective short
+                // options and flags anyway
+                if (strcmp(argv[optind-1], "--help") == 0);
+                else if (strcmp(argv[optind-1], "--left") == 0);
+                else if (strcmp(argv[optind-1], "--middle") == 0);
+                else if (strcmp(argv[optind-1], "--right") == 0);
+                else if (strcmp(argv[optind-1], "--docker") == 0);
+                else if (strcmp(argv[optind-1], "--no-dashes") == 0);
+                else if (strcmp(argv[optind-1], "--separator") == 0);
                 // handle optional argument for --ubuntu option
-                if (strcmp(argv[optind-1], "--ubuntu") == 0) {
+                else if (strcmp(argv[optind-1], "--ubuntu") == 0) {
                     if (argv[optind]) {
                         if (argv[optind][0] == '-'); // next arg is an option
                         else if (strlen(argv[optind]) == 1 && argv[optind][0] >= 'a' && argv[optind][0] <= 'z') {
@@ -70,8 +82,7 @@ int main(int argc, char** argv) {
                         }
                         
                     }
-                }
-                if (strcmp(argv[optind-2], "--left-start") == 0) {
+                } else if (strcmp(argv[optind-2], "--left-start") == 0) {
                     if (strlen(optarg) == 1 && optarg[0] >= 'a' && optarg[0] <= 'z') {
                         left_start = optarg[0];
                     } else {
@@ -80,8 +91,7 @@ int main(int argc, char** argv) {
                         usage();
                         exit(EXIT_FAILURE);
                     }
-                }
-                if (strcmp(argv[optind-2], "--middle-start") == 0) {
+                } else if (strcmp(argv[optind-2], "--middle-start") == 0) {
                     if (strlen(optarg) == 1 && optarg[0] >= 'a' && optarg[0] <= 'z') {
                         middle_start = optarg[0];
                     } else {
@@ -90,8 +100,7 @@ int main(int argc, char** argv) {
                         usage();
                         exit(EXIT_FAILURE);
                     }
-                }
-                if (strcmp(argv[optind-2], "--right-start") == 0) {
+                } else if (strcmp(argv[optind-2], "--right-start") == 0) {
                     if (strlen(optarg) == 1 && optarg[0] >= 'a' && optarg[0] <= 'z') {
                         right_start = optarg[0];
                     } else {
@@ -100,6 +109,11 @@ int main(int argc, char** argv) {
                         usage();
                         exit(EXIT_FAILURE);
                     }
+                } else {
+                    // no valid long-only options found
+                    fprintf(stderr, "ERROR: Invalid option (%s)\n\n", argv[optind-1]);
+                    usage();
+                    exit(EXIT_FAILURE);
                 }
                 break;
             case 'h': // User requested help
